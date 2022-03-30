15450e3b2f72d6ee80414732fef94631168ce499
startithr <102576826+startithr@users.noreply.github.com>
2022-03-30 19:19:07 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const config = {
+  style: 'mapbox://styles/infostartit/cl0l3y9l6000b14n2qnjsfik6',
+  accessToken:
+    'pk.eyJ1IjoiaW5mb3N0YXJ0aXQiLCJhIjoiY2t6MnFkb3NxMDBmYjJudG00dDlodWl6cSJ9.8ABWFSKEAzbuVzZcwkbxfQ',
+  CSV: './Data.csv',
+  center: [17.825963778361903, 43.33872113674153],
+  zoom: 7,
+  title: 'Dekanati i župni uredi',
+  description:
+    'Ovdje je popis dekanata i župnih ureda po biskupijama. Filtrira se tako da - lorem ipsum',
+  sideBarInfo: ['Zupni_ured', 'Naziv', 'Mjesto'],
+  popupInfo: ['URL'],
+  filters: [
+    {
+      type: 'dropdown',
+      title: 'Odaberite biskupiju: ',
+      columnHeader: 'Biskupija',
+      listItems: [
+        'Mostarsko-duvanjska biskupija',
+        'Trebinjsko-mrkanska biskupija',
+      ],
+    },
+   {
+      type: 'dropdown',
+      title: 'Odaberite dekanat: ',
+      columnHeader: 'Dekanat',
+      listItems: [
+        'Broćanski dekanat',
+        'Čapljinski dekanat',
+        'Duvanjski dekanat',
+        'Grudski dekanat',
+        'Ljubuški dekanat',
+        'Mostarski dekanat',
+        'Posuški dekanat',
+        'Širokobriješki dekanat',
+        'Stolački dekanat',
+        'Trebinjski dekanat',
+      ],
+    },
+  ],
+};
