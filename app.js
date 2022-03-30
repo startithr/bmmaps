a408d13eed7d016266b15391d2f2dd6077360533
startithr <102576826+startithr@users.noreply.github.com>
2022-03-30 18:32:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -455,8 +455,8 @@ map.on('load', () => {
             data: geojsonData,
           },
           paint: {
-            'circle-radius': 5, // size of circles
-            'circle-color': '#3D2E5D', // color of circles
+            'circle-radius': 6, // size of circles
+            'circle-color': '#A12A2A', // color of circles
             'circle-stroke-color': 'white',
             'circle-stroke-width': 1,
             'circle-opacity': 0.7,
