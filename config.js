17544611478045ea2becfaca83e8ae7e70600259
startithr <102576826+startithr@users.noreply.github.com>
2022-04-06 19:47:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,20 +6,14 @@ const config = {
   accessToken:
     'pk.eyJ1IjoiaW5mb3N0YXJ0aXQiLCJhIjoiY2t6MnFkb3NxMDBmYjJudG00dDlodWl6cSJ9.8ABWFSKEAzbuVzZcwkbxfQ',
   CSV: './Data.csv',
-  center: [17.825963778361903, 43.33872113674153],
+  center: [16.632916834626496, 43.78831073687657],
   zoom: 7,
-  title: 'Karta Mostarsko-duvanjske i Trebinjsko-mrkanske biskupije',
+  title: 'Karta Mostarsko-duvanjske biskupije',
   description:
     'Popis i položaj župnih ureda. ',
   sideBarInfo: ['Zupni_ured', 'Naziv', 'Mjesto'],
   popupInfo: ['URL'],
   filters: [
-   {
-      type: 'dropdown',
-      title: 'Odaberite biskupiju: ',
-      columnHeader: 'Biskupija', // Case sensitive - must match spreadsheet entry
-      listItems: ['Mostarsko-duvanjska biskupija', 'Trebinjsko-mrkanska biskupija'], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
-   },
    {
       type: 'dropdown',
       title: 'Odaberite dekanat: ',
@@ -33,8 +27,6 @@ const config = {
         'Mostarski dekanat',
         'Posuški dekanat',
         'Širokobriješki dekanat',
-        'Stolački dekanat',
-        'Trebinjski dekanat',
       ],
     },
   ],
